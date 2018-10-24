8109ea490e6cb0a1d4d4ce50abdee32ca8536e02
hendraramadani <hendraramadani14@gmail.com>
2018-10-24 09:55:32 +0700

    Update Edit.php

--- Edit.php
+++ Edit.php
@@ -15,7 +15,7 @@ while ($user_data = mysqli_fetch_array($result)){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>TCourier : Sign Up</title>
+  <title>TCourier : Edit User</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
